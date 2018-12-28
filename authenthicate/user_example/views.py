2e58b1b5734314aada0b0cccc3b67c650acd1787
ahmadabd <ahmadabd13741112@gmail.com>
2018-12-28 23:42:55 +0330

    working on many things :O

--- authenthicate/user_example/views.py
+++ authenthicate/user_example/views.py
@@ -3,28 +3,32 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 
+from django.views.decorators.csrf import csrf_exempt
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from .forms import SignUpForm
+
 # Create your views here.
 
+@csrf_exempt
+@login_required
 def index(request):
     count = User.objects.count()        ### Count number of users that are login
     return render(request, 'user_example/index.html', { 'count' : count})
 
-
+@csrf_exempt
 def register(request):
-
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username = username, password = password)
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
             login(request, user)
-
-            return redirect('index')
+            return redirect('/')
     else:
-        form = UserCreationForm()
-
-    context = {'form' : form}
-    return render(request, 'registration/register.html', context)
+        form = SignUpForm()
+    return render(request, 'registration/register.html', {'form': form})
